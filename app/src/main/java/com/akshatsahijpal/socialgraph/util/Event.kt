d0cfa9a2fdc3275cf310e44c8b5bbe9c065abbdd
akshat-sahijpal <20akshatsahijpal01droid01@gmail.com>
2021-06-19 03:07:30 +0530

    version 1.0

--- app/src/main/java/com/akshatsahijpal/socialgraph/util/Event.kt
+++ app/src/main/java/com/akshatsahijpal/socialgraph/util/Event.kt
@@ -1,21 +1,16 @@
 package com.akshatsahijpal.socialgraph.util
-
 import androidx.lifecycle.Observer
-
 class Event<out T>(private var data: T) {
     var isHandled = false
         private set
-
     fun getContentIfNotHandled(): T? {
         return if (!isHandled) {
             isHandled = true
             data
         } else null
     }
-
     fun peek() = data
 }
-
 class EventObserver<T>(
     private inline val onError: ((err: String) -> Unit)? = null,
     private inline val onSuccess: ((T) -> Unit),
